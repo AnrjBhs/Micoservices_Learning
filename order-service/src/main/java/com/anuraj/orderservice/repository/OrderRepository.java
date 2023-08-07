802750e2a8f5562563613003ef630254c73ddd4b
Anuraj <iamanubhosale@gmail.com>
2023-08-07 22:55:59 +0530

    deleted order service from main

--- order-service/src/main/java/com/anuraj/orderservice/repository/OrderRepository.java
+++
@@ -1,8 +0,0 @@
-package com.anuraj.orderservice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.anuraj.orderservice.entity.Order;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
